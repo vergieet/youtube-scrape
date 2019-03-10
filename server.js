f4d9140fe574dfe771450b34e9d2f9592b1e5b15
Vergie Ericson T <vergieericsont@gmail.com>
2019-03-10 14:16:20 +0700

    Update server.js
    adding cors support

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
+var cors = require('cors')
+
+app.use(cors())
 
 //Home page
 app.get('/', function(req, res) {
